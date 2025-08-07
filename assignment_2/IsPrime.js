2fd7a4e1c10e071d68d93fcb34298f0e1687903f
Yasura Lakshitha <lakshitha200087@gmail.com>
2025-08-07 12:39:09 +0530

    improve isPrime for decimal places

--- assignment_2/IsPrime.js
+++ assignment_2/IsPrime.js
@@ -1,5 +1,5 @@
 function isPrime(number) {
-    if (number <= 1) return false;
+    if (number <= 1 || number%1!=0) return false;
 
     for (let i = 2; i < number; i++)
         if (number % i == 0) return false;
