d8c0393f623801567f20239e3cfbd4a05580a0d4
Yasura Lakshitha <lakshitha200087@gmail.com>
2025-08-07 12:29:32 +0530

    implement isPrime function

---
+++ assignment_2/IsPrime.js
@@ -0,0 +1,22 @@
+function isPrime(number) {
+    if (number <= 1) return false;
+
+    for (let i = 2; i < number; i++)
+        if (number % i == 0) return false;
+    return true;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById('btn-submit').addEventListener('click', () => {
+        const inputNumber = document.getElementById("number-input").value;
+
+        let result = "";
+
+        inputNumber == "" ?
+            result = "Input number is not provided" :
+            result = isPrime(inputNumber) ? inputNumber + " is a prime number" :
+                     inputNumber + " is not a prime number";
+
+        document.getElementById('output').textContent = result;
+    })
+})
