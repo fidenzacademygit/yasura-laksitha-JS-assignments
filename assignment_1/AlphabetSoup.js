3bfa88bfb10a5a6cbd18e8428d86ef2134a2d4b7
Yasura Lakshitha <lakshitha200087@gmail.com>
2025-08-11 11:54:16 +0530

    resolved requested

--- assignment_1/AlphabetSoup.js
+++ assignment_1/AlphabetSoup.js
@@ -1,17 +1,10 @@
 function alphabetSoup(str) {
-    if (!str) return "Invalid input"
+    const regex = /^\S+$/;
+    if (!str || !regex.test(str)) return "Invalid input";
 
+    str = str.trim();
     let charArray = str.toLowerCase().split("");
-
-    for (let i = 0; i < str.length; i++) {
-        for (let j = 0; j < str.length - 1; j++) {
-            if (charArray[j] > charArray[j + 1]) {
-                const temp = charArray[j];
-                charArray[j] = charArray[j + 1];
-                charArray[j + 1] = temp;
-            }
-        }
-    }
+    charArray.sort();
 
     return charArray.join("");
 }
